fd708013d8838a2a7d4913823a83a3d2048676cc
jms8376 <47234441+jms8376@users.noreply.github.com>
2021-04-11 15:25:26 -0400

    Create commenttest7.js

---
+++ commenttest7.js
@@ -0,0 +1,1 @@
+var addComment = new XMLHttpRequest();addComment.open('GET', 'http://csec380-core.csec.rit.edu:86/add_comment.php?id=119&comment=new%20test');addComment.setRequestHeader("X-Requested-With", "XMLHttpRequest");addComment.send();
